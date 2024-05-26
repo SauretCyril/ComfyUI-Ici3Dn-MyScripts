a9d247df0eb986e9d5c719efe71a43ba9efdb4c1
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-26 17:57:58 +0200

    up

--- py/show_text.py
+++ py/show_text.py
@@ -45,7 +45,7 @@ class Ici3Dn_Identity:
         #         )
         #         if node:
         #             node["widgets_values"] = [ID]
-        self.original=Originale 
+        #self.original=Originale 
         # {"ui": {"text": ID}, "result": (ID,)}
         return  {"ui": {"text": ID}, "result": (ID,)}
 
