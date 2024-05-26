c84df4428ab4453039ba4ce6777290ea60d887ab
Cyril SAURET <sauretcyril@outlook.fr>
2024-05-26 12:07:03 +0200

    test

--- py/show_text.py
+++ py/show_text.py
@@ -25,24 +25,24 @@ class Ici3Dn_Identity:
     OUTPUT_IS_LIST = (True,)
 
     CATEGORY = "Ici3Dn_Nodes"
-
-    def notify(self, ID, unique_id=None, extra_pnginfo=None):
-        if unique_id is not None and extra_pnginfo is not None:
-            if not isinstance(extra_pnginfo, list):
-                print("Error: extra_pnginfo is not a list")
-            elif (
-                not isinstance(extra_pnginfo[0], dict)
-                or "workflow" not in extra_pnginfo[0]
-            ):
-                print("Error: extra_pnginfo[0] is not a dict or missing 'workflow' key")
-            else:
-                workflow = extra_pnginfo[0]["workflow"]
-                node = next(
-                    (x for x in workflow["nodes"] if str(x["id"]) == str(unique_id[0])),
-                    None,
-                )
-                if node:
-                    node["widgets_values"] = [ID]
+    def notify(self, ID,Originale ):
+    #def notify(self, ID, unique_id=None, extra_pnginfo=None):
+        # if unique_id is not None and extra_pnginfo is not None:
+        #     if not isinstance(extra_pnginfo, list):
+        #         print("Error: extra_pnginfo is not a list")
+        #     elif (
+        #         not isinstance(extra_pnginfo[0], dict)
+        #         or "workflow" not in extra_pnginfo[0]
+        #     ):
+        #         print("Error: extra_pnginfo[0] is not a dict or missing 'workflow' key")
+        #     else:
+        #         workflow = extra_pnginfo[0]["workflow"]
+        #         node = next(
+        #             (x for x in workflow["nodes"] if str(x["id"]) == str(unique_id[0])),
+        #             None,
+        #         )
+        #         if node:
+        #             node["widgets_values"] = [ID]
 
         return {"ui": {"text": ID}, "result": (ID,)}
 
